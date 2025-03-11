42ba6f40d6875c9c4d569796fc6bb94b6672851c
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2025-03-11 15:22:41 +0300

    bloqage des pages selon le rôle

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,15 +4,40 @@ import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Router } from '@angular/router';
 
+export interface User {
+  id: number;
+  username: string;
+  email: string;
+  role: string;
+  token: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient,private router: Router,) { }
+  private currentUser: User | null = null;
+  
+  constructor(private http: HttpClient, private router: Router,) {
+    this.loadUserFromLocalStorage();
+   }
 
   login(credentials: { email: string; password: string }): Observable<any> {
     return this.http.post(`${environment.baseUrl}/login`, credentials);
   }
+
+  // login(credentials: { email: string; password: string }): Observable<User> {
+  //   return new Observable<User>(observer => {
+  //     this.http.post<User>(`${environment.baseUrl}/login`, credentials)
+  //       .subscribe({
+  //         next: (user) => {
+  //           this.storeUser(user);
+  //           observer.next(user);
+  //         },
+  //         error: (error) => observer.error(error)
+  //       });
+  //   });
+  // }
 
   signup(userData: { username: string; email: string; password: string; }): Observable<any> {
     return this.http.post(`${environment.baseUrl}/signup`, userData);
@@ -20,6 +45,33 @@ export class AuthService {
 
   logout(): void {
     localStorage.removeItem('carcare-token'); // ou le nom de ton token
+    localStorage.removeItem('carcare-user');
     this.router.navigate(['/authentication/login']); // ou une autre page publique
+  }
+
+  hasRole(requiredRole: string): boolean {
+    if (!this.currentUser) return false;
+    return this.currentUser.role === requiredRole;
+  }
+
+  private storeUser(user: User): void {
+    this.currentUser = user;
+    localStorage.setItem('carcare-token', user.token);
+    localStorage.setItem('carcare-user', JSON.stringify(user));
+  }
+
+  private loadUserFromLocalStorage() : void{
+    const storedUser = localStorage.getItem('carcare-user')
+    if(storedUser){
+      this.currentUser = JSON.parse(storedUser)
+    }
+  }
+
+  get currentUserValue(): User | null {
+    return this.currentUser;
+  }
+
+  get isLoggedIn(): boolean {
+    return this.currentUser !== null;
   }
 }
