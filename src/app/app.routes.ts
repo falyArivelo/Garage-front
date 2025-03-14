63e8bbed8ffbf5833719d5e07c41824a902d1e7d
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2025-03-14 14:44:03 +0300

    role management end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { BlankComponent } from './layouts/blank/blank.component';
 import { FullComponent } from './layouts/full/full.component';
 import { AuthGuard } from './guards/auth.guard';
 import { RoleGuard } from './guards/role.guard';
+import { UnauthorizedComponent } from './pages/unauthorized/unauthorized.component';
 
 export const routes: Routes = [
   {
@@ -54,4 +55,8 @@ export const routes: Routes = [
     path: '**',
     redirectTo: 'authentication/error',
   },
+  {
+    path: 'unauthorized',
+    component: UnauthorizedComponent,
+  }
 ];
