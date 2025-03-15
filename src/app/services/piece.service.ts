5fc29db0d2a764df139ba24a318fe412a1a2073f
Nancyrandria <99555595+Nancyrandria@users.noreply.github.com>
2025-03-15 18:57:38 +0100

    add app/services/piece

---
+++ src/app/services/piece.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { environment } from '../../environments/environment' // Import de l’environnement
+
+@Injectable({
+    providedIn: 'root',
+})
+
+export class PieceService {
+    private apiUrl = `${environment.baseUrl}/pieces` // Utilisation de la variable d’environnement
+    constructor(private http: HttpClient) {}
+    // Liste toutes les pieces
+    getPieces(): Observable<any> {
+      return this.http.get(this.apiUrl)
+    }
+    // Ajout piece
+    addPiece(piece: any): Observable<any> {
+      return this.http.post(this.apiUrl, piece)
+    }
+    // Modifie par piece
+    updatePiece(id: string, piece: any): Observable<any> {
+      return this.http.put(`${this.apiUrl}/${id}`, piece)
+    }
+    // Supprime une piece
+    deletePiece(id: string): Observable<any> {
+      return this.http.delete(`${this.apiUrl}/${id}`)
+    }
+}
