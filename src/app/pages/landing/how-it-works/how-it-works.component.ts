ee3687106efa46f6541523d9e129de3d89dcc27d
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2025-03-19 21:31:48 +0300

    squellete landing page

---
+++ src/app/pages/landing/how-it-works/how-it-works.component.ts
@@ -0,0 +1,18 @@
+// import { Component } from '@angular/core';
+// import { MatDialogRef } from '@angular/material/dialog';
+// import { MatDialogModule } from '@angular/material/dialog';
+// @Component({
+  
+//   selector: 'app-how-it-works',
+//   imports: [ ],
+//   templateUrl: './how-it-works.component.html',
+//   styleUrl: './how-it-works.component.scss'
+// })
+// export class HowItWorksComponent {
+//   constructor(public dialogRef: MatDialogRef<HowItWorksComponent>) {}
+
+//   close(): void {
+//     this.dialogRef.close();
+//   }
+// }
+
