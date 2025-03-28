99e7561fced43eec40d400595620ff873df48733
Nancyrandria <99555595+Nancyrandria@users.noreply.github.com>
2025-03-28 16:15:50 +0100

    Create send Email and configuration

--- src/app/pages/email/sendEmail/sendEmail.component.ts
+++ src/app/pages/email/sendEmail/sendEmail.component.ts
@@ -9,6 +9,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { EditorModule } from '@tinymce/tinymce-angular';
 import { EmailService } from 'src/app/services/email.service';
 
 @Component({
@@ -26,7 +27,7 @@ import { EmailService } from 'src/app/services/email.service';
         MatInputModule,
         MatCheckboxModule,
         MatProgressBarModule,
-        FormsModule
+        EditorModule, // Ajout du module TinyMCE
       ],
   templateUrl: './sendEmail.component.html',
   styleUrls: ['./sendEmail.component.scss'],
@@ -37,6 +38,18 @@ export class EmailComponent {
   text = '';
 
   constructor(private emailService: EmailService) {}
+
+  // Configuration de TinyMCE
+  editorConfig = {
+    base_url: '/assets/tinymce',  // Change /tinymce en /assets/tinymce
+    suffix: '.min',
+    plugins: 'lists link image autoresize',
+    toolbar: 'undo redo | formatselect | bold italic underline | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent',
+    menubar: false,
+    height: 300,
+    placeholder: 'Veuillez écrire votre message ici...'  // Ajout du placeholder ici
+  };
+  
 
   sendEmail() {
     console.log('Données envoyées:', { to: this.to, subject: this.subject, text: this.text });
